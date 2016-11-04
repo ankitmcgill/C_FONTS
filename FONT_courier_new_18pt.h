c3c79551f6c4948a31517f60ec241d4947c2599d
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-11-04 11:38:11 +0530

    added courier new 18pt

---
+++ FONT_courier_new_18pt.h
@@ -0,0 +1,4 @@
+/* Font data for Courier New 18pt */
+extern const uint_8 courierNew_18ptBitmaps[];
+extern const FONT_INFO courierNew_18ptFontInfo;
+extern const FONT_CHAR_INFO courierNew_18ptDescriptors[];
