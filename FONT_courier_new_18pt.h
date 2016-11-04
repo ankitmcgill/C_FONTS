8b02873a187d63034433d177cf3c87b3c13e4518
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-11-04 16:36:26 +0530

    added new font ROLANDE 36pt

--- FONT_courier_new_18pt.h
+++ FONT_courier_new_18pt.h
@@ -1,4 +1,12 @@
+#ifndef _FONT_COURIER_NEW_18PT_
+#define _FONT_COURIER_NEW_18PT_
+
+#include "user_interface.h"
+#include <ets_sys.h>
+#include <osapi.h>
+
 /* Font data for Courier New 18pt */
-extern const uint_8 courierNew_18ptBitmaps[];
-extern const FONT_INFO courierNew_18ptFontInfo;
-extern const FONT_CHAR_INFO courierNew_18ptDescriptors[];
+extern const uint8_t courierNew_18ptBitmaps[];
+extern const uint16_t courierNew_18ptDescriptors[][3];
+
+#endif
