836707885832961641533ad063e7626b7ab7799d
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-11-05 21:41:15 +0530

    added new font files

---
+++ FONT_rolande_28pt.h
@@ -0,0 +1,12 @@
+#ifndef _FONT_ROLANDE_36PT_
+#define _FONT_ROLANDE_36PT_
+
+#include "user_interface.h"
+#include <ets_sys.h>
+#include <osapi.h>
+
+// Font data for Rolande 36pt
+extern const uint8_t rolande_36ptBitmaps[];
+extern const uint16_t rolande_36ptDescriptors[][3];
+
+#endif
