577d666694a18a323bc0e553bb652e2868f9ef1b
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2017-04-04 12:13:28 +0530

    Added 10pt Menlo Font

---
+++ FONT_menlo_10pt.h
@@ -0,0 +1,10 @@
+#ifndef _FONT_MENLO_10PT_
+#define _FONT_MENLO_10PT_
+
+#include <stdint.h>
+
+/* Font data for Courier New 18pt */
+extern const uint8_t menlo_10ptBitmaps[];
+extern const uint16_t menlo_10ptDescriptors[][3];
+
+#endif
